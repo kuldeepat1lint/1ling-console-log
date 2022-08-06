f08955a991b056777db36d1b4a34b0fb9bcfcf38
kuldeep <hi@kuldeep.tech>
2022-08-06 13:26:48 +0530

    added onelint namespace for extension config

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,40 @@
-import * as vscode from "vscode";
-import { DebugMessage } from "./debug-message";
-import { JSDebugMessage } from "./debug-message/js";
-import { ExtensionProperties, Message } from "./entities";
-import { LineCodeProcessing } from "./line-code-processing";
-import { JSLineCodeProcessing } from "./line-code-processing/js";
+import * as vscode from 'vscode'
+import { DebugMessage } from './debug-message'
+import { JSDebugMessage } from './debug-message/js'
+import { ExtensionProperties, Message } from './entities'
+import { LineCodeProcessing } from './line-code-processing'
+import { JSLineCodeProcessing } from './line-code-processing/js'
 
 export function activate(context: vscode.ExtensionContext) {
-  const jsLineCodeProcessing: LineCodeProcessing = new JSLineCodeProcessing();
-  const jsDebugMessage: DebugMessage = new JSDebugMessage(jsLineCodeProcessing);
+  const jsLineCodeProcessing: LineCodeProcessing = new JSLineCodeProcessing()
+  const jsDebugMessage: DebugMessage = new JSDebugMessage(jsLineCodeProcessing)
   // Insert debug message
   vscode.commands.registerCommand(
-    "turboConsoleLog.displayLogMessage",
+    'OnelintConsoleLog.displayLogMessage',
     async () => {
       const editor: vscode.TextEditor | undefined =
-        vscode.window.activeTextEditor;
+        vscode.window.activeTextEditor
       if (!editor) {
-        return;
+        return
       }
-      const tabSize: number | string = getTabSize(editor.options.tabSize);
-      const document: vscode.TextDocument = editor.document;
+      const tabSize: number | string = getTabSize(editor.options.tabSize)
+      const document: vscode.TextDocument = editor.document
       const config: vscode.WorkspaceConfiguration =
-        vscode.workspace.getConfiguration("turboConsoleLog");
-      const properties: ExtensionProperties = getExtensionProperties(config);
+        vscode.workspace.getConfiguration('OnelintConsoleLog')
+      const properties: ExtensionProperties = getExtensionProperties(config)
       for (let index = 0; index < editor.selections.length; index++) {
-        const selection: vscode.Selection = editor.selections[index];
-        
-        let wordUnderCursor = "";
-        const rangeUnderCursor: vscode.Range | undefined = document.getWordRangeAtPosition(
-            selection.active
-        );
+        const selection: vscode.Selection = editor.selections[index]
+
+        let wordUnderCursor = ''
+        const rangeUnderCursor: vscode.Range | undefined =
+          document.getWordRangeAtPosition(selection.active)
         // if rangeUnderCursor is undefined, `document.getText(undefined)` will return the entire file.
         if (rangeUnderCursor) {
-            wordUnderCursor = document.getText(rangeUnderCursor);
+          wordUnderCursor = document.getText(rangeUnderCursor)
         }
-        const selectedVar: string = document.getText(selection) || wordUnderCursor;
-        const lineOfSelectedVar: number = selection.active.line;
+        const selectedVar: string =
+          document.getText(selection) || wordUnderCursor
+        const lineOfSelectedVar: number = selection.active.line
         // Check if the selection line is not the last one in the document and the selected variable is not empty
         if (selectedVar.trim().length !== 0) {
           const logType = 'log'
@@ -48,7 +48,7 @@ export function activate(context: vscode.ExtensionContext) {
             insertEnclosingFunction,
             delimiterInsideMessage,
             includeFileNameAndLineNum,
-          } = properties;
+          } = properties
           await editor.edit((editBuilder) => {
             jsDebugMessage.msg(
               editBuilder,
@@ -66,39 +66,39 @@ export function activate(context: vscode.ExtensionContext) {
               tabSize,
               logType,
               logFunction
-            );
-          });
+            )
+          })
         }
       }
     }
-  );
+  )
   //insert console.error()
   vscode.commands.registerCommand(
-    "turboConsoleLog.displayErrorMessage",
+    'OnelintConsoleLog.displayErrorMessage',
     async () => {
       const editor: vscode.TextEditor | undefined =
-        vscode.window.activeTextEditor;
+        vscode.window.activeTextEditor
       if (!editor) {
-        return;
+        return
       }
-      const tabSize: number | string = getTabSize(editor.options.tabSize);
-      const document: vscode.TextDocument = editor.document;
+      const tabSize: number | string = getTabSize(editor.options.tabSize)
+      const document: vscode.TextDocument = editor.document
       const config: vscode.WorkspaceConfiguration =
-        vscode.workspace.getConfiguration("turboConsoleLog");
-      const properties: ExtensionProperties = getExtensionProperties(config);
+        vscode.workspace.getConfiguration('OnelintConsoleLog')
+      const properties: ExtensionProperties = getExtensionProperties(config)
       for (let index = 0; index < editor.selections.length; index++) {
-        const selection: vscode.Selection = editor.selections[index];
-        
-        let wordUnderCursor = "";
-        const rangeUnderCursor: vscode.Range | undefined = document.getWordRangeAtPosition(
-            selection.active
-        );
+        const selection: vscode.Selection = editor.selections[index]
+
+        let wordUnderCursor = ''
+        const rangeUnderCursor: vscode.Range | undefined =
+          document.getWordRangeAtPosition(selection.active)
         // if rangeUnderCursor is undefined, `document.getText(undefined)` will return the entire file.
         if (rangeUnderCursor) {
-            wordUnderCursor = document.getText(rangeUnderCursor);
+          wordUnderCursor = document.getText(rangeUnderCursor)
         }
-        const selectedVar: string = document.getText(selection) || wordUnderCursor;
-        const lineOfSelectedVar: number = selection.active.line;
+        const selectedVar: string =
+          document.getText(selection) || wordUnderCursor
+        const lineOfSelectedVar: number = selection.active.line
         // Check if the selection line is not the last one in the document and the selected variable is not empty
         if (selectedVar.trim().length !== 0) {
           const logType = 'error'
@@ -112,7 +112,7 @@ export function activate(context: vscode.ExtensionContext) {
             insertEnclosingFunction,
             delimiterInsideMessage,
             includeFileNameAndLineNum,
-          } = properties;
+          } = properties
           await editor.edit((editBuilder) => {
             jsDebugMessage.msg(
               editBuilder,
@@ -130,39 +130,39 @@ export function activate(context: vscode.ExtensionContext) {
               tabSize,
               logType,
               logFunction
-            );
-          });
+            )
+          })
         }
       }
     }
-  );
-   //insert console.table()
-   vscode.commands.registerCommand(
-    "turboConsoleLog.displayTableLogMessage",
+  )
+  //insert console.table()
+  vscode.commands.registerCommand(
+    'OnelintConsoleLog.displayTableLogMessage',
     async () => {
       const editor: vscode.TextEditor | undefined =
-        vscode.window.activeTextEditor;
+        vscode.window.activeTextEditor
       if (!editor) {
-        return;
+        return
       }
-      const tabSize: number | string = getTabSize(editor.options.tabSize);
-      const document: vscode.TextDocument = editor.document;
+      const tabSize: number | string = getTabSize(editor.options.tabSize)
+      const document: vscode.TextDocument = editor.document
       const config: vscode.WorkspaceConfiguration =
-        vscode.workspace.getConfiguration("turboConsoleLog");
-      const properties: ExtensionProperties = getExtensionProperties(config);
+        vscode.workspace.getConfiguration('OnelintConsoleLog')
+      const properties: ExtensionProperties = getExtensionProperties(config)
       for (let index = 0; index < editor.selections.length; index++) {
-        const selection: vscode.Selection = editor.selections[index];
-        
-        let wordUnderCursor = "";
-        const rangeUnderCursor: vscode.Range | undefined = document.getWordRangeAtPosition(
-            selection.active
-        );
+        const selection: vscode.Selection = editor.selections[index]
+
+        let wordUnderCursor = ''
+        const rangeUnderCursor: vscode.Range | undefined =
+          document.getWordRangeAtPosition(selection.active)
         // if rangeUnderCursor is undefined, `document.getText(undefined)` will return the entire file.
         if (rangeUnderCursor) {
-            wordUnderCursor = document.getText(rangeUnderCursor);
+          wordUnderCursor = document.getText(rangeUnderCursor)
         }
-        const selectedVar: string = document.getText(selection) || wordUnderCursor;
-        const lineOfSelectedVar: number = selection.active.line;
+        const selectedVar: string =
+          document.getText(selection) || wordUnderCursor
+        const lineOfSelectedVar: number = selection.active.line
         // Check if the selection line is not the last one in the document and the selected variable is not empty
         if (selectedVar.trim().length !== 0) {
           const logType = 'table'
@@ -176,7 +176,7 @@ export function activate(context: vscode.ExtensionContext) {
             insertEnclosingFunction,
             delimiterInsideMessage,
             includeFileNameAndLineNum,
-          } = properties;
+          } = properties
           await editor.edit((editBuilder) => {
             jsDebugMessage.msg(
               editBuilder,
@@ -194,107 +194,107 @@ export function activate(context: vscode.ExtensionContext) {
               tabSize,
               logType,
               logFunction
-            );
-          });
+            )
+          })
         }
       }
     }
-  );
+  )
   // Comment all debug messages
   vscode.commands.registerCommand(
-    "turboConsoleLog.commentAllLogMessages",
+    'OnelintConsoleLog.commentAllLogMessages',
     () => {
       const editor: vscode.TextEditor | undefined =
-        vscode.window.activeTextEditor;
+        vscode.window.activeTextEditor
       if (!editor) {
-        return;
+        return
       }
-      const tabSize: number = getTabSize(editor.options.tabSize);
-      const document: vscode.TextDocument = editor.document;
+      const tabSize: number = getTabSize(editor.options.tabSize)
+      const document: vscode.TextDocument = editor.document
       const config: vscode.WorkspaceConfiguration =
-        vscode.workspace.getConfiguration("turboConsoleLog");
-      const properties: ExtensionProperties = getExtensionProperties(config);
+        vscode.workspace.getConfiguration('OnelintConsoleLog')
+      const properties: ExtensionProperties = getExtensionProperties(config)
       const logMessages: Message[] = jsDebugMessage.detectAll(
         document,
         tabSize,
         properties.delimiterInsideMessage,
         properties.quote
-      );
+      )
       editor.edit((editBuilder) => {
         logMessages.forEach(({ spaces, lines }) => {
           lines.forEach((line: vscode.Range) => {
-            editBuilder.delete(line);
+            editBuilder.delete(line)
             editBuilder.insert(
               new vscode.Position(line.start.line, 0),
               `${spaces}// ${document.getText(line).trim()}\n`
-            );
-          });
-        });
-      });
+            )
+          })
+        })
+      })
     }
-  );
+  )
   // Uncomment all debug messages
   vscode.commands.registerCommand(
-    "turboConsoleLog.uncommentAllLogMessages",
+    'OnelintConsoleLog.uncommentAllLogMessages',
     () => {
       const editor: vscode.TextEditor | undefined =
-        vscode.window.activeTextEditor;
+        vscode.window.activeTextEditor
       if (!editor) {
-        return;
+        return
       }
-      const tabSize: number = getTabSize(editor.options.tabSize);
-      const document: vscode.TextDocument = editor.document;
+      const tabSize: number = getTabSize(editor.options.tabSize)
+      const document: vscode.TextDocument = editor.document
       const config: vscode.WorkspaceConfiguration =
-        vscode.workspace.getConfiguration("turboConsoleLog");
-      const properties: ExtensionProperties = getExtensionProperties(config);
+        vscode.workspace.getConfiguration('OnelintConsoleLog')
+      const properties: ExtensionProperties = getExtensionProperties(config)
       const logMessages: Message[] = jsDebugMessage.detectAll(
         document,
         tabSize,
         properties.delimiterInsideMessage,
         properties.quote
-      );
+      )
       editor.edit((editBuilder) => {
         logMessages.forEach(({ spaces, lines }) => {
           lines.forEach((line: vscode.Range) => {
-            editBuilder.delete(line);
+            editBuilder.delete(line)
             editBuilder.insert(
               new vscode.Position(line.start.line, 0),
-              `${spaces}${document.getText(line).replace(/\//g, "").trim()}\n`
-            );
-          });
-        });
-      });
+              `${spaces}${document.getText(line).replace(/\//g, '').trim()}\n`
+            )
+          })
+        })
+      })
     }
-  );
+  )
   // Delete all debug messages
   vscode.commands.registerCommand(
-    "turboConsoleLog.deleteAllLogMessages",
+    'OnelintConsoleLog.deleteAllLogMessages',
     () => {
       const editor: vscode.TextEditor | undefined =
-        vscode.window.activeTextEditor;
+        vscode.window.activeTextEditor
       if (!editor) {
-        return;
+        return
       }
-      const tabSize: number = getTabSize(editor.options.tabSize);
-      const document: vscode.TextDocument = editor.document;
+      const tabSize: number = getTabSize(editor.options.tabSize)
+      const document: vscode.TextDocument = editor.document
       const config: vscode.WorkspaceConfiguration =
-        vscode.workspace.getConfiguration("turboConsoleLog");
-      const properties: ExtensionProperties = getExtensionProperties(config);
+        vscode.workspace.getConfiguration('OnelintConsoleLog')
+      const properties: ExtensionProperties = getExtensionProperties(config)
       const logMessages: Message[] = jsDebugMessage.detectAll(
         document,
         tabSize,
         properties.delimiterInsideMessage,
         properties.quote
-      );
+      )
       editor.edit((editBuilder) => {
         logMessages.forEach(({ lines }) => {
           lines.forEach((line: vscode.Range) => {
-            editBuilder.delete(line);
-          });
-        });
-      });
+            editBuilder.delete(line)
+          })
+        })
+      })
     }
-  );
+  )
 }
 
 export function deactivate() {}
@@ -302,19 +302,19 @@ export function deactivate() {}
 function getExtensionProperties(
   workspaceConfig: vscode.WorkspaceConfiguration
 ) {
-  const wrapLogMessage = workspaceConfig.wrapLogMessage || false;
+  const wrapLogMessage = workspaceConfig.wrapLogMessage || false
   const logMessagePrefix = workspaceConfig.logMessagePrefix
     ? workspaceConfig.logMessagePrefix
-    : "";
-  const addSemicolonInTheEnd = workspaceConfig.addSemicolonInTheEnd || false;
-  const insertEnclosingClass = workspaceConfig.insertEnclosingClass;
-  const insertEnclosingFunction = workspaceConfig.insertEnclosingFunction;
-  const quote = workspaceConfig.quote || '"';
-  const delimiterInsideMessage = workspaceConfig.delimiterInsideMessage || "~";
+    : ''
+  const addSemicolonInTheEnd = workspaceConfig.addSemicolonInTheEnd || false
+  const insertEnclosingClass = workspaceConfig.insertEnclosingClass
+  const insertEnclosingFunction = workspaceConfig.insertEnclosingFunction
+  const quote = workspaceConfig.quote || '"'
+  const delimiterInsideMessage = workspaceConfig.delimiterInsideMessage || '~'
   const includeFileNameAndLineNum =
-    workspaceConfig.includeFileNameAndLineNum || false;
-  const logType = workspaceConfig.logType || "log";
-  const logFunction = workspaceConfig.logFunction || 'log';
+    workspaceConfig.includeFileNameAndLineNum || false
+  const logType = workspaceConfig.logType || 'log'
+  const logFunction = workspaceConfig.logFunction || 'log'
   const extensionProperties: ExtensionProperties = {
     wrapLogMessage,
     logMessagePrefix,
@@ -325,17 +325,17 @@ function getExtensionProperties(
     delimiterInsideMessage,
     includeFileNameAndLineNum,
     logType,
-    logFunction
-  };
-  return extensionProperties;
+    logFunction,
+  }
+  return extensionProperties
 }
 
 function getTabSize(tabSize: string | number | undefined): number {
-  if (tabSize && typeof tabSize === "number") {
-    return tabSize;
-  } else if (tabSize && typeof tabSize === "string") {
-    return parseInt(tabSize);
+  if (tabSize && typeof tabSize === 'number') {
+    return tabSize
+  } else if (tabSize && typeof tabSize === 'string') {
+    return parseInt(tabSize)
   } else {
-    return 4;
+    return 4
   }
 }
